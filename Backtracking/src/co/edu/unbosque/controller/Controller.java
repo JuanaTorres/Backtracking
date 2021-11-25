f7e3f2758e8c66829cacbb4a5bbb0b75ed56f240
Andres <a.galvisbolivar@gmail.com>
2021-11-25 11:45:57 -0500

    menu

--- Backtracking/src/co/edu/unbosque/controller/Controller.java
+++ Backtracking/src/co/edu/unbosque/controller/Controller.java
@@ -4,12 +4,32 @@ import co.edu.unbosque.view.View;
 
 public class Controller {
 	private View vista;
+
 	public Controller() {
-		vista= new View();
+		vista = new View();
 		funcionar();
 	}
+
 	public void funcionar() {
-		int num= vista.leerDatoEntero("ingrese numero de elementos de serie de fibonacci");
-		
+		int a = vista.leerDatoEntero(
+				"Por favor ingresa que cual de los algoritmos quieres hacer \n1. Nutricionista va a un restaurante(Backtracking) \n2. Liga de futbol Colombiana (Backtracking)\n3. Saltos de un conejo(Rama y Poda)\n4 Cualquier otro numero para salir");
+		switch (a) {
+		case 1: {
+
+			break;
+		}
+		case 2: {
+
+			break;
+		}
+		case 3: {
+
+			break;
+		}
+		default:
+			vista.imprimirMensaje("¡Hasta luego!");
+			System.exit(0);
+		}
+
 	}
 }
